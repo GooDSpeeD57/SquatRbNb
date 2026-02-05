fc65a0f5f08da5325d2e66dd92fb1c3d203aeab3
GooDSpeeD57 <juju.taesch@gmail.com>
2026-02-05 11:45:19 +0100

    first commit

--- src/main/java/training/afpa/cda24060/squartrbnb/repository/UserRepository.java
+++ src/main/java/training/afpa/cda24060/squartrbnb/repository/UserRepository.java
@@ -3,6 +3,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import training.afpa.cda24060.squartrbnb.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findById(Integer id);
 }
